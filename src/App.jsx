bc63ae1c5a46cf747aa17d5f9ceb1297d84417ee
skullacho <pedrosmbsilva@gmail.com>
2026-05-12 20:18:13 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,178 @@
+import "./App.css";
+
+function App() {
+  return (
+    <table border="1">
+      <tr>
+        <th>ID</th>
+        <th>Nome Completo</th>
+        <th>Email</th>
+        <th>Senha</th>
+        <th>Telefone</th>
+        <th>Cidade</th>
+        <th>Estado</th>
+        <th>Profissão</th>
+        <th>Idade</th>
+        <th>Data de Nascimento</th>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>Lucas Andrade Silva</td>
+        <td>lucas.andrade@gmail.com</td>
+        <td>lucas123</td>
+        <td>(19) 99999-1111</td>
+        <td>Campinas</td>
+        <td>SP</td>
+        <td>Designer</td>
+        <td>18</td>
+        <td>15/08/2007</td>
+      </tr>
+
+      <tr>
+        <td>2</td>
+        <td>Mariana Costa Oliveira</td>
+        <td>mariana.costa@gmail.com</td>
+        <td>mari2025</td>
+        <td>(11) 98888-2222</td>
+        <td>São Paulo</td>
+        <td>SP</td>
+        <td>Programadora</td>
+        <td>19</td>
+        <td>03/11/2006</td>
+      </tr>
+
+      <tr>
+        <td>3</td>
+        <td>Pedro Henrique Souza</td>
+        <td>pedro.souza@gmail.com</td>
+        <td>pedro321</td>
+        <td>(21) 97777-3333</td>
+        <td>Rio de Janeiro</td>
+        <td>RJ</td>
+        <td>Analista</td>
+        <td>20</td>
+        <td>27/04/2005</td>
+      </tr>
+
+      <tr>
+        <td>4</td>
+        <td>Ana Beatriz Lima</td>
+        <td>ana.lima@gmail.com</td>
+        <td>ana456</td>
+        <td>(31) 96666-4444</td>
+        <td>Belo Horizonte</td>
+        <td>MG</td>
+        <td>Estudante</td>
+        <td>17</td>
+        <td>09/01/2008</td>
+      </tr>
+
+      <tr>
+        <td>5</td>
+        <td>Gabriel Ferreira Santos</td>
+        <td>gabriel.fs@gmail.com</td>
+        <td>gabs789</td>
+        <td>(41) 95555-5555</td>
+        <td>Curitiba</td>
+        <td>PR</td>
+        <td>Engenheiro</td>
+        <td>18</td>
+        <td>18/06/2007</td>
+      </tr>
+
+      <tr>
+        <td>6</td>
+        <td>Juliana Martins Rocha</td>
+        <td>juliana.rocha@gmail.com</td>
+        <td>juju999</td>
+        <td>(51) 94444-6666</td>
+        <td>Porto Alegre</td>
+        <td>RS</td>
+        <td>Arquiteta</td>
+        <td>19</td>
+        <td>12/02/2006</td>
+      </tr>
+
+      <tr>
+        <td>7</td>
+        <td>Rafael Almeida Costa</td>
+        <td>rafael.costa@gmail.com</td>
+        <td>rafa1234</td>
+        <td>(71) 93333-7777</td>
+        <td>Salvador</td>
+        <td>BA</td>
+        <td>Professor</td>
+        <td>20</td>
+        <td>30/09/2005</td>
+      </tr>
+
+      <tr>
+        <td>8</td>
+        <td>Camila Fernandes Melo</td>
+        <td>camila.melo@gmail.com</td>
+        <td>cami321</td>
+        <td>(81) 92222-8888</td>
+        <td>Recife</td>
+        <td>PE</td>
+        <td>Médica</td>
+        <td>18</td>
+        <td>25/12/2007</td>
+      </tr>
+
+      <tr>
+        <td>9</td>
+        <td>Bruno Henrique Alves</td>
+        <td>bruno.alves@gmail.com</td>
+        <td>bruno777</td>
+        <td>(61) 91111-9999</td>
+        <td>Brasília</td>
+        <td>DF</td>
+        <td>Empresário</td>
+        <td>19</td>
+        <td>07/05/2006</td>
+      </tr>
+
+      <tr>
+        <td>10</td>
+        <td>Fernanda Ribeiro Gomes</td>
+        <td>fernanda.r@gmail.com</td>
+        <td>fefe2024</td>
+        <td>(85) 90000-1010</td>
+        <td>Fortaleza</td>
+        <td>CE</td>
+        <td>Jornalista</td>
+        <td>17</td>
+        <td>14/10/2008</td>
+      </tr>
+
+      <tr>
+        <td>11</td>
+        <td>Thiago Lopes Martins</td>
+        <td>thiago.lopes@gmail.com</td>
+        <td>thiago001</td>
+        <td>(27) 98888-2020</td>
+        <td>Vitória</td>
+        <td>ES</td>
+        <td>Administrador</td>
+        <td>20</td>
+        <td>21/03/2005</td>
+      </tr>
+
+      <tr>
+        <td>12</td>
+        <td>Beatriz Carvalho Pinto</td>
+        <td>beatriz.cp@gmail.com</td>
+        <td>bibi852</td>
+        <td>(62) 97777-3030</td>
+        <td>Goiânia</td>
+        <td>GO</td>
+        <td>Fotógrafa</td>
+        <td>18</td>
+        <td>11/07/2007</td>
+      </tr>
+    </table>
+  );
+}
+
+export default App;
